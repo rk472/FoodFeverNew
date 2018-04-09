9e76a56174f3a5ecbcbff876ea8e49681311ae5e
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-04-09 20:05:30 +0530

    Items Style Chaneged

--- app/src/main/java/com/studio/smarters/foodfever/ProfileActivity.java
+++ app/src/main/java/com/studio/smarters/foodfever/ProfileActivity.java
@@ -1,11 +1,15 @@
 package com.studio.smarters.foodfever;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -15,15 +19,23 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
-    TextView nameText,phoneText,emailText;
-    Button historyButton,logOut;
-    FirebaseAuth mAuth;
-    String uid,name,email,phone;
-    DatabaseReference d;
+    private TextView nameText,phoneText,emailText;
+    private Button historyButton,logOut;
+    private FirebaseAuth mAuth;
+    private String uid,name,email,phone;
+    private DatabaseReference d;
+    private ProgressDialog pd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        //Progress Dialog
+        pd = new ProgressDialog(this);
+        pd.setTitle("Please Wait");
+        pd.setMessage("Loading Your Profile");
+        pd.setCanceledOnTouchOutside(false);
+        pd.show();
+
         nameText=findViewById(R.id.profile_name);
         phoneText=findViewById(R.id.profile_phone);
         emailText=findViewById(R.id.profile_email);
@@ -41,21 +53,31 @@ public class ProfileActivity extends AppCompatActivity {
                 nameText.setText(name);
                 emailText.setText(email);
                 phoneText.setText(phone);
+                pd.dismiss();
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Toast.makeText(ProfileActivity.this, "Error Loading Profile", Toast.LENGTH_SHORT).show();
+                finish();
+                pd.dismiss();
             }
         });
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAuth.signOut();
-                Intent i=new Intent(ProfileActivity.this,LoginActivity.class);
-                startActivity(i);
-                finishAffinity();
-                finish();
+                new AlertDialog.Builder(ProfileActivity.this)
+                        .setTitle("Log Out")
+                        .setMessage("Do You really want to LogOut ?")
+                        .setPositiveButton("Yes, Sure", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                mAuth.signOut();
+                                startActivity(new Intent(ProfileActivity.this,LoginActivity.class));
+                                finishAffinity();
+                                finish();
+                            }
+                        }).setNegativeButton("No, Don't",null).show();
             }
         });
         historyButton.setOnClickListener(new View.OnClickListener() {
@@ -65,5 +87,10 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
